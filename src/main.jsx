f2a4b65708fa665fd40d04de833259cf27576991
alailayangan27-png <alailayangan27@gmail.com>
2026-04-17 13:55:36 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <TonConnectUIProvider manifestUrl="https://mint-ton-app-kkfd.vercel.app/tonconnect-manifest.json">
-    <App />
-  </TonConnectUIProvider>
-);
+<TonConnectUIProvider manifestUrl="https://mint-ton-app-kkfd.vercel.app/tonconnect-manifest.json">
