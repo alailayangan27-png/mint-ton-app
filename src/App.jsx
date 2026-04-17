5b1aa3b6956623aa468a65f0a3552d3e46377cfc
alailayangan27-png <alailayangan27@gmail.com>
2026-04-17 13:05:13 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
+
+export default function App() {
+  const [tonConnectUI] = useTonConnectUI();
+
+  const mint = async () => {
+    try {
+      await tonConnectUI.sendTransaction({
+        validUntil: Math.floor(Date.now() / 1000) + 60,
+        messages: [
+          {
+            address: "EQXXXXXXXXXXXXXXX", // GANTI WALLET KAMU
+            amount: "100000000" // 0.1 TON
+          }
+        ]
+      });
+    } catch (e) {
+      console.log("User cancel / error");
+    }
+  };
+
+  return (
+    <div style={{
+      color:"#fff",
+      textAlign:"center",
+      paddingTop:"50px"
+    }}>
+      <h1>Mint Token</h1>
+
+      <TonConnectButton />
+
+      <button
+        onClick={mint}
+        style={{
+          marginTop:"30px",
+          padding:"15px 25px",
+          fontSize:"18px",
+          background:"#00f2ff",
+          border:"none",
+          borderRadius:"10px"
+        }}
+      >
+        Mint (0.1 TON)
+      </button>
+    </div>
+  );
+}
